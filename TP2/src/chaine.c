82a23f7d72d35905e20c5ce654541dd0b77e577e
jeanne.claret <jeanne.claret@cpe.fr>
2022-09-26 09:55:55 +0200

    fibo + chaine

---
+++ TP2/src/chaine.c
@@ -0,0 +1,15 @@
+ /*
+Sans utiliser les bibliothèques standards ou externes (par exemple
+string.h), écrivez le code *chaine.c* qui
+
+1.  calcule le nombre de caractères dans une chaine de caractères
+2.  copie une chaine de caractères dans une autre chaine de caractère
+3.  concatène deux chaines de caractères
+*/
+
+#include <stdio.h>
+
+int main() {
+    char chaine = 'hallelujah';
+
+}
