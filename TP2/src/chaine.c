76d3c678941120f333f6e3fba9712bea35855391
jeanne.claret <jeanne.claret@cpe.fr>
2022-09-26 10:05:05 +0200

    dfdfhcfhdu

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -1,15 +1,29 @@
- /*
-Sans utiliser les bibliothèques standards ou externes (par exemple
-string.h), écrivez le code *chaine.c* qui
-
-1.  calcule le nombre de caractères dans une chaine de caractères
-2.  copie une chaine de caractères dans une autre chaine de caractère
-3.  concatène deux chaines de caractères
-*/
-
 #include <stdio.h>
 
 int main() {
-    char chaine = 'hallelujah';
+    char mot[] = "hi";
+    char mot_copie[sizeof(mot)/sizeof(char)+1]; /* taille du mot copie comme etant celle du mot + le caractere NULL -> sizeof(char) = 1  */
+    char mot_autre[]="Guys" ;                     
+    char mot_concatene[(sizeof(mot)+sizeof(mot_autre))+1]; /* On definit une chaine de caractere de la taille de mot_autre + mot */
+    
+    int j;
+    int i;
+    for(i=0; mot[i] != '\0'; i++) {
+     /* On compte la taille de mot (meme si on pouvait utiliser sizeof(mot) comme plus haut) */
+        mot_copie[i] = mot[i];
+        mot_concatene[i] =mot[i];
+    }
+    mot_copie[sizeof(mot)-1] = '\0'; /*On enlève le caractere NULL de mot pour ajouter celui de mot_copie */
 
+    for(j=0; j < sizeof(mot_autre); j++) { 
+    /* Boucle pour la concatenation de mot_autre puis de mot */
+        
+        mot_concatene[j+sizeof(mot)-1]= mot_autre[j];    /* -1 pour enlever le NULL de mot */
+    }
+   
+
+    printf("Nombre de caractere de la chaine : %i \n",i);
+    printf("mot copie : %s \n",mot_copie);
+    printf("mot concatene: %s \n",mot_concatene);
+    
 }
--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -10,17 +10,17 @@ Fibonacci U0, U1, U2,...Un. */
 #include <stdio.h>
 
 int main() {
-    int n= 11; /* choix du nombre d'iterations */
+    int n= 19; /* choix du nombre d'iterations */
     int U0 = 0;
     int U1 = 1; /* definition des variables de debut */
     
-    disp("Les %i termes  ", n)
+   int U_next;
     
-    for(n, n>1, n--) {
-        U_next = U1 + U0;
+    for(n; n>1; n--) {
+        U_next = U1 + U0; /*calcul du terme de Fibonacci */
         U0 = U1;
-        U1 = U_next;
-        disp("de la suite de Fibonacci sont : %i \n ", U_next)
+        U1 = U_next; /* changement des valeurs des variables n-1 et n-2 */
+        printf("les n termes de la suite de Fibonacci sont : %i \n ", U_next);
     }
 
     
