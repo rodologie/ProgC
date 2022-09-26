a6470d328a4f788afd3260db929a2bba783b00a7
jeanne.claret <jeanne.claret@cpe.fr>
2022-09-26 12:14:49 +0200

    tableauptr fonctionne pas

---
+++ TP2/src/tableauptr.c
@@ -0,0 +1,27 @@
+/* FONCTIONNE PAS */
+
+#include <stdio.h>
+
+int main() {
+
+    char tableau1[7] = {1,2,5,6,7,8,9};
+    char tableau2[8]= {1.02,2.3,4.5,7.6,8.1,2.36,1.56,6.45};
+
+    int k;
+    for(k=0; k>8;k++) {
+        short *ptr1 = &tableau1[k];
+        printf("Tab 1 :Les positions initiales de la valeur %i sont : %hd \n", k,ptr1[k]);
+        short *ptr2 = &tableau2[k];
+        printf("Tab 2 : Les positions initiales de la valeur %i sont : %hd \n", k,ptr2[k]);
+        if(*ptr1 % 2 ==0) {
+            ptr1 = ptr1 + 3;
+            printf("Tab 1 :La valeur est divisible par 2, sa position est : %hd \n", ptr1[k]);
+        }
+        if(*ptr2 % 2 ==0){
+            ptr2 = ptr2 + 3;
+            printf("Tab 2 : La valeur est divisible par 2, sa position est : %hd \n", ptr2[k]);
+        }
+    }
+
+return 0;
+}
