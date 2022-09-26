9be403c936b218b93b014a608e99452efa8d6aea
Rodolphe Lajugie <ggrodizz@cpe.fr>
2022-09-26 09:09:10 +0200

    bits

---
+++ TP2/src/bits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int d = 0b00111011000000000001011;
+    int test = 0b00010000000000000001000;
+    int res = d & test;
+
+    if (res == test) {
+        printf("%d\n", 1);
+    }
+    else {
+        printf("%d\n", 0);
+    }
+}
