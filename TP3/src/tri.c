4e21773a2930459ce30e7f14d3a41bcca24e8604
Rodolphe Lajugie <ggrodizz@cpe.fr>
2022-10-03 09:35:17 +0200

    tri ok

--- TP3/src/tri.c
+++ TP3/src/tri.c
@@ -2,9 +2,29 @@
 
 int main() {
 
-int N = 100;
-int T[100];
-int rand();
-int k;
+    int rand();
+    int k,j;
+    int tableau_init[100];
+    int inter;
 
+    for (k = 0; k < sizeof(tableau_init) / sizeof(int); k++){
+        tableau_init[k] = rand()%100; /* Parcourt de tous les indices du tableau pour assigner une valeur à chaque élément*/
+    }
 
+    for (k=99; k> 0; k--){
+        for (j=98; j > 0; j--){
+            if (tableau_init[j]>tableau_init[j+1]){
+                inter = tableau_init[j+1];
+                tableau_init[j+1]=tableau_init[j];
+                tableau_init[j] = inter;
+            }
+        }
+    }
+    printf("Tableau trié:"); 
+    
+    for (k = 0; k < sizeof(tableau_init) / sizeof(int); k++)
+    {
+        printf("%i\n", tableau_init[k]); /* Parcourt de tous les indices du tableau pour assigner une valeur à chaque élément*/
+    }
+    return 0;
+}
