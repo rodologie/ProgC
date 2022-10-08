ff6583086f23da1c6ff2c915c78da042a7fa9f9b
Rodolphe Lajugie <ggrodizz@cpe.fr>
2022-10-08 15:05:21 +0200

    finition ex4

--- TP3/src/chercher.c
+++ TP3/src/chercher.c
@@ -11,6 +11,8 @@ int main() {
     int k;
     int tableau[100]; /* Declaration d'un tableau de 100 valeurs */
     int nombre_recherche;
+    int err;
+    int prst = 0;
 
     int entree;
     printf("Entrez la valeur de l'entier recherché entre 0 et 150 : ");
@@ -22,18 +24,31 @@ int main() {
         tableau[k] = rand() % 150;    /* Attribution aleatoire des entiers du tableau */                         
        
     }
+    
 
     for(j=0;j<100;j++) {
 
         if(tableau[j]== nombre_recherche) { /* Recherche de l'entier dans le tableau */
-            printf("entier present \n");
+            prst = prst + 1;
+        }
+        else{
+            err = err + 1;
         }
     }
     }
+    if (prst >= 1){
+        printf("Entier present %i fois\n", prst);
+    }
+    else if ((prst == 0) || (err == 100))
+    {
+        printf("Entier non présent \n");
+    }
+    
+    
 
     // -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_ Exercice 8 -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-    
+
 
 
     return 0;
