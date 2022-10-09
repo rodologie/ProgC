246e21108ce395bd313846243a6f8dfb7bc9b79b
Rodolphe Lajugie <ggrodizz@cpe.fr>
2022-10-09 21:32:49 +0200

    fin comment

--- TP3/src/recherche_dichotomique.c
+++ TP3/src/recherche_dichotomique.c
@@ -47,9 +47,9 @@ int main()
     while (val_rech != tableau_init[indice]){ //boucle tant que la valeur recherchée est différente de celle pointée dans le tableau
         if (val_rech > tableau_init[indice]){ //si valeur recherchée est plus grande que celle pointée
              borne_min = indice;    //la borne min devient l'indice de la valeur pointée
-             indice = (borne_max+borne_min)/2;  //l'indice devient la valuer moyenne 
-             if ((val_rech != tableau_init[indice])&&(val_rech < tableau_init[indice + 1])&& (borne_min == indice)){
-                err  =1;
+             indice = (borne_max+borne_min)/2;  //l'indice devient la valeur moyenne 
+             if ((val_rech != tableau_init[indice])&&(val_rech < tableau_init[indice + 1])&& (borne_min == indice)){ //Si la valeur pointée n'est pas celle qui est recherchée, qu'elle est plus grande que celle pointée et plus petie que celle à l'indice suivant
+                err  =1;                                                                                             //alors on fixe l'erreur à 1 pour éviter de boucler indéfiniment
                 break;
              }
         }
@@ -57,8 +57,12 @@ int main()
         {
             borne_max = indice;
             indice = (borne_max + borne_min) / 2;
+            if ((val_rech != tableau_init[indice]) && (val_rech > tableau_init[indice - 1]) && (borne_max == indice)){// Si la valeur pointée n'est pas celle qui est recherchée, qu'elle est plus peite que celle pointée et plus grande que celle à l'indice précédent
+                err = 1;                                                                                              // alors on fixe l'erreur à 1 pour éviter de boucler indéfiniment
+                break;
+            }
         }
-        else if ((k == -1)||(indice == -1)){
+        else if ((k == -1)||(indice == -1)){ //si on dépasse la longueur du tableauo u qu'on boucle trop (normalement pas nécésaire)
             err = 1;
             break;
         }
@@ -69,10 +73,10 @@ int main()
         
         
     }
-    if (err == 1) {
+    if (err == 1) { //si erreur on indique qu'il y a erreur
         printf("la valeur n'est pas dans le tableau \n");
     }
-    else if (err == 0)
+    else if (err == 0) // si pas d'erreur on affiche la valeur trouvée et l'indice
     {
         printf("valeur recherchée = %d, valeur trouvée = %d, à l'indice %d \n", val_rech, tableau_init[indice], indice);
     }
--- TP3/src/tri.c
+++ TP3/src/tri.c
@@ -11,9 +11,9 @@ int main() {
         tableau_init[k] = rand()%100; /* Parcourt de tous les indices du tableau pour assigner une valeur à chaque élément*/
     }
 
-    for (k=99; k> 0; k--){
-        for (j=98; j > 0; j--){
-            if (tableau_init[j]>tableau_init[j+1]){
+    for (k=99; k> 0; k--){ //Pour chaque nombre du tableau
+        for (j=98; j > 0; j--){ //On boucle au maximum 98 fois si le premier élément est le plus grand 
+            if (tableau_init[j]>tableau_init[j+1]){ // si l'élément est plus grand que celui d'apres, alors on les échanges
                 inter = tableau_init[j+1];
                 tableau_init[j+1]=tableau_init[j];
                 tableau_init[j] = inter;
@@ -22,9 +22,9 @@ int main() {
     }
     printf("Tableau trié:"); 
     
-    for (k = 0; k < sizeof(tableau_init) / sizeof(int); k++)
+    for (k = 0; k < sizeof(tableau_init) / sizeof(int); k++) // on boucle pour affixher chaque élément du tableau trié
     {
-        printf("%i\n", tableau_init[k]); /* Parcourt de tous les indices du tableau pour assigner une valeur à chaque élément*/
+        printf("%i\n", tableau_init[k]); 
     }
     return 0;
 }
