f379e1c544f5451ae1f7249d4de1e951dca916da
Rodolphe Lajugie <ggrodizz@cpe.fr>
2022-10-10 08:59:21 +0200

    debut ex1

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -10,7 +10,7 @@ int main() {
     int reste_division = a%b;
     printf("Les résultats sont:\n - Addition: %d,\n - Soustraction: %d,\n - Multiplication: %d, \n - Division: %f, \n - Reste de la division euclidienne: %d \n", addition, soustraction, multiplication, division, reste_division);
 
-    int negation = !a;
+    int negation = ~a;
     int ET = a && b;
     int OU = a||b;
     printf("Les résulatats des tests logiques sont:\n - Négation: %d,\n - ET: %d\n - OU: %d\n", negation, ET, OU);
---
+++ TP4/src/operator.h
@@ -0,0 +1,31 @@
+int addition(int num1, int num2) {
+    return num1 + num2;
+}
+
+int soustraction(int num1, int num2){
+    return  num1 - num2;
+}
+
+int produit(int num1, int num2){
+    return num1*num2;
+}
+
+int quotient(int num1, int num2){
+    return num1/num2;
+}
+
+int modulo(int num1, int num2){
+    return num1%num2;
+}
+
+int ET(int num1, int num2){
+    return num1&&num2;
+}
+
+int OU(int num1, int num2){
+    return num1 || num2;
+}
+
+int negation(int num1, int num2){
+    return ~num1;
+}
