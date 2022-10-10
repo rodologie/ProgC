3b7aa8edce996e11870a23bd7b29f9653fa78382
Rodolphe Lajugie <rodolphe.lajugie@cpe.fr>
2022-10-10 12:03:45 +0200

    ca marche pas

--- TP4/src/calcule.c
+++ TP4/src/calcule.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "operator.h"
 
-int main(int argc, char argv[] ){
+int main(int argc, char *argv[] ){
     int res;
-    int num1;
-    num1 = atoi(argv[2]);
-    int num2 = atoi(argv[3]);
+    printf("%s\n",*argv[0]);
+    printf("%s\n",*argv[1]);
+    printf("%s\n",*argv[2]);
+    printf("%s\n",*argv[3]);
+    int num1 = atoi(*argv[2]);
+    int num2 = atoi(*argv[3]);
     
-    switch (argv[1]){
+  /*  switch (*argv[1]){
         case '+':  res = addition(num1,num2);  // addition
             break;
         case '-':  res = soustraction(num1,num2);  // soustraction
@@ -24,7 +28,7 @@ int main(int argc, char argv[] ){
             break;
         case '~':  res = negation(num1,num2);   //contraire (non x)
             break;
-    }
+    }*/
     printf("%d \n",res);
     return 0;
 }
