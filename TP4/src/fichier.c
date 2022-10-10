a4737d909facd1bd6c625ff8ec391b6ae29a8ac0
Jeanne <jeanne.claret@cpe.fr>
2022-10-10 10:37:05 +0200

    fichier.h et .c

--- TP4/src/fichier.c
+++ TP4/src/fichier.c
@@ -1,3 +1,33 @@
-Bonjour Rodolphe,
+/* déclarez les fonctions lire_fichier(char
+\*nom_de_fichier) et ecrire_dans_fichier(char \*nom_de_fichier,
+char \*message). La fonction lire_fichier prend le nom du fichier et
+affiche son contenu à l'écran. ecrire_dans_fichier prend le nom du
+fichier et écrire le message saisi par l'utilisateur dans le fichier. */
+#include <stdio.h> 
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-Nous sommes en TP distanciel ce lundi  10 octobre matin.
+char lire_fichier(char *nom_fichier) {
+    // Fonction renvoyant les caracteres presents dans le fichier 
+    char content[]; // Definition de du contenu du fichier 
+    int fd, count, size; // declaration des entiers utiles
+    fd = open ("./nom_fichier", O_RDONLY); // ouverture du fichier : fichier.docx
+    size = read(fd, content, sizeof(content));
+    for (count = 0; count < size; count ++) {
+        printf("%c", content[count]); 
+    }
+    close(fd);
+    return 0; 
+}; 
+  
+char ecrire_dans_fichier(char *nom_fichier,  char *message ) {
+    char message_utilisateur[]; 
+    printf("Entrez la phrase que vous souhaitez ajouter : ");
+    scanf("%s", message_utilisateur);
+    char content[] = message_utilisateur ;
+    int fd, count, size;
+    fd = open ("./nom_fichier", O_CREAT|O_WRONLY|O_APPEND, S_IRUSR|S_IWUSR); size = write(fd, content, sizeof(content));
+    close(fd);
+    return 0;
+};
--- TP4/src/fichier.h
+++ TP4/src/fichier.h
@@ -1,29 +1,8 @@
-/* déclarez les fonctions lire_fichier(char
-\*nom_de_fichier) et ecrire_dans_fichier(char \*nom_de_fichier,
-char \*message). La fonction lire_fichier prend le nom du fichier et
-affiche son contenu à l'écran. ecrire_dans_fichier prend le nom du
-fichier et écrire le message saisi par l'utilisateur dans le fichier. */
-
 #include <stdio.h> 
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-void lire_fichier(char *fichier.c) {
+char lire_fichier(char *fichier.docx) 
 
-}; 
-
-void ecrire_dans_fichier(char *fichier.c,  char *message ) {
-    char content[1000];
-    int fd, count, size;
-    fd = open ("./file.c", O_RDONLY);
-    size = read(fd, content, 1000);
-    for (count = 0; count < size; count ++) {
-        printf("%c", content[count]); 
-    }
-    close(fd);
-    return 0; 
-};
-
-int main(int argc, char ** argv) { 
-}
+void ecrire_dans_fichier(char *fichier.c,  char *message ) 
