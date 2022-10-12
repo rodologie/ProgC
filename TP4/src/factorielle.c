742d085b85d3b4e856bfb19577dbfd62d6b5c37e
Rodolphe Lajugie <rodolphe.lajugie@cpe.fr>
2022-10-12 14:01:14 +0200

    Factp ++

--- TP4/src/factorielle.c
+++ TP4/src/factorielle.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 int factorielle (int num) { // Fonction récursive pour calculer les factorielles
-  if (num == 0) {
-    return 1; // 0! = 1
-  }
-  else {
-    return (num * factorielle (num-1)); // On rappelle la fonction pour l'entier-1 jusqu'à arriver à 0
-  }
+    if (num == 0) {
+        return 1; // 0! = 1
+        }
+    else {
+        return (num * factorielle (num-1)); // On rappelle la fonction pour l'entier-1 jusqu'à arriver à 0
+        }
 }
 int main () { // Appel et affichage du résultat de la fonction
-    int i= 4;
-    printf(" %i !  = %i \n \n",i,factorielle(i));
+    for (int k= 10; k>0; k--){
+        printf(" %i !  = %i \n \n",k,factorielle(k));
+    }
 
 return 0;
 }
+
+//on rajoute une boucle for à l'affichage 
+//afin de pouvoir afficher plusieurs factorielle
