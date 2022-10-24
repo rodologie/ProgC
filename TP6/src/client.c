f0cadf0daad0157e7ca834591623b8e51d79283c
jeanne.claret <jeanne.claret@cpe.fr>
2022-10-24 09:43:37 +0200

    mmm:mm

--- TP6/src/client.c
+++ TP6/src/client.c
@@ -108,12 +108,12 @@ int envoie_couleurs(int socketfd, char *pathname,int nb_couleurs)
 
 int main(int argc, char **argv)
 {
-  int N;
-  char nb_couleurs[10];
+  int nb_couleurs;
+  char N[10];
   printf("Choisissez le nombre de couleurs a afficher <= 30 : ");
   fgets(nb_couleurs,3,stdin);
-  N=atoi(nb_couleurs);
-  
+  nb_couleurs=atoi(N);
+
   int socketfd;
 
   struct sockaddr_in server_addr;
